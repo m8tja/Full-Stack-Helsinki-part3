b116de8500e3c779501bae797cb5639eddde7675
m8tja <mc5620@student.uni-lj.si>
2023-02-18 20:29:59 +0100

    exercise 3.9.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const morgan = require("morgan")
+app.use(cors())
 app.use(express.json())
 //app.use(morgan("tiny"))
 
