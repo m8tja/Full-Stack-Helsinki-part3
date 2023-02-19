77c325508e82c5342334431d2b3699463710483f
m8tja <mc5620@student.uni-lj.si>
2023-02-19 16:39:49 +0100

    exercises 3.9.-3.11.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 const morgan = require("morgan")
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 //app.use(morgan("tiny"))
 
 morgan.token("data", (request, response) => {
